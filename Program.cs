a7db1a2fb9cfbf973e6c642c679323c43276da61
Gabi Guarini <gabibea97@gmail.com>
2020-02-07 08:38:16 -0300

    the code only
    fibonacci series from an n input number

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first=0,second=1,A,i,n;
+   
+            Console.Write("Ingrese un nro: ");
+            n = Convert.ToInt32(Console.ReadLine());    
+            Console.Write("Estos son {0} números de la serie de Fibonacci : \n",n);
+            Console.Write("{0}  {1}", first,second);
+            
+            for(i=3;i<=n;i++)
+            {
+                A=first+second;
+                Console.Write(" {0} ",A);
+                first=second;
+                second=A;
+            }
+            Console.Write("\n");
+            
+        }
+    }
+}
